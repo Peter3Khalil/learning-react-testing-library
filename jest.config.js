bdf308cd85c7d1198c561824757b6b442e3048bf
Peter Khalil <peter.khalil.fahmy@gmail.com>
2025-02-04 12:13:01 +0200

    ✨ Update Jest configuration for improved testing experience

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,10 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({
   dir: './',
 });
-
+/** @type {import('@jest/types').Config.InitialOptions} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
+  verbose: true,
 };
 module.exports = createJestConfig(customJestConfig);
